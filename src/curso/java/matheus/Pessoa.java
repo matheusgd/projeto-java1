58d4dbc8d019921647cf792d2428a3b801078734
Matheus <matheus.gdias1@gmail.com>
2021-05-26 21:30:47 -0300

    Alteração e criação da classe Pessoa

---
+++ src/curso/java/matheus/Pessoa.java
@@ -0,0 +1,22 @@
+package curso.java.matheus;
+
+public class Pessoa {
+    private String nome;
+    private String sobrenome;
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+}
