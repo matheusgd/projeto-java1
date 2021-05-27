86da628c5547cfa74cb4119f16e19be65a379b87
Matheus <matheus.gdias1@gmail.com>
2021-05-26 21:41:25 -0300

    Organização dos Getters e Setters dentro da Classe Pessoa

--- src/curso/java/matheus/Pessoa.java
+++ src/curso/java/matheus/Pessoa.java
@@ -1,32 +1,30 @@
 package curso.java.matheus;
 
 public class Pessoa {
+
     private String nome;
     private String sobrenome;
-
     private int idade;
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public void setSobrenome(String sobrenome) {
-        this.sobrenome = sobrenome;
-    }
 
     public String getNome() {
         return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
     }
 
     public String getSobrenome() {
         return sobrenome;
     }
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
 }
