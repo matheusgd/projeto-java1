b881f3702f56b8e3db7a60997005718f30a18e13
Matheus <matheus.gdias1@gmail.com>
2021-05-26 22:11:01 -0300

    Revert "Adicionando campo Data de Nascimento 2"
    This reverts commit d9b32e52

--- src/curso/java/matheus/Pessoa.java
+++ src/curso/java/matheus/Pessoa.java
@@ -8,7 +8,6 @@ public class Pessoa {
     private String sobrenome;
     private int idade;
     private Date dataNascimento;
-    private Date dataNascimento2;
 
     public String getNome() {
         return nome;
