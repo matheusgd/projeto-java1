e93b19765f6fa067e4ebd38c4802b6e809fa31db
Matheus <matheus.gdias1@gmail.com>
2021-06-02 17:20:41 -0300

    Adicionando Estrutura For

---
+++ src/curso/java/matheus/EstruturaFor.java
@@ -0,0 +1,15 @@
+/* Estrutura de Repetição FOR [Módulo 8.7] */
+package curso.java.matheus;
+
+public class EstruturaFor {
+
+    public static void main(String[] args) {
+
+        for (int numero = 10; numero >= 0; numero --){
+            System.out.println("Número atual é: " + numero);
+
+        }
+
+    }
+
+}
