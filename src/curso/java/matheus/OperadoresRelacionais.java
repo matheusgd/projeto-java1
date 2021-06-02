4c1cbe9be70b16e8eb062967076dd3a1fecb3719
Matheus <matheus.gdias1@gmail.com>
2021-06-02 16:59:25 -0300

    Adicionando Operadores Relacionais

---
+++ src/curso/java/matheus/OperadoresRelacionais.java
@@ -0,0 +1,25 @@
+/*Operadores Relacionais [Modulo 8.5]
+== Igual
+!= Diferente
+< Menor
+> Maior
+<= Menor ou Igual
+>= Maior ou Igual */
+package curso.java.matheus;
+
+public class OperadoresRelacionais {
+
+    public static void main(String[] args) {
+
+        int nota1 = 90;
+        int nota2 = 90;
+
+        if (nota1 == nota2){
+            System.out.println("As Notas são iguais!");
+        } else {
+            System.out.println("As notas são diferentes!");
+        }
+
+    }
+
+}
