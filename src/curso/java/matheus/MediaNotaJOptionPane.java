3f7a7a5e52ba9c3d08bd7b7b4e4cba3c0536430e
Matheus <matheus.gdias1@gmail.com>
2021-06-02 19:19:30 -0300

    Adicionando JOptionPane Programa Media Nota Bimestral

---
+++ src/curso/java/matheus/MediaNotaJOptionPane.java
@@ -0,0 +1,39 @@
+package curso.java.matheus;
+
+import javax.swing.*;
+
+public class MediaNotaJOptionPane {
+
+    public static void main(String[] args) {
+
+        String nota1 = JOptionPane.showInputDialog("Informe a nota do 1º Bimestre: ");
+        String nota2 = JOptionPane.showInputDialog("Informe a nota do 2º Bimestre: ");
+        String nota3 = JOptionPane.showInputDialog("Informe a nota do 3º Bimestre: ");
+        String nota4 = JOptionPane.showInputDialog("Informe a nota do 4º Bimestre: ");
+
+        double dNota1 = Double.parseDouble(nota1);
+        double dNota2 = Double.parseDouble(nota2);
+        double dNota3 = Double.parseDouble(nota3);
+        double dNota4 = Double.parseDouble(nota4);
+
+        double media = (dNota1 + dNota2 + dNota3 + dNota4) / 4;
+
+        int resposta = JOptionPane.showConfirmDialog(null,"Deseja ver sua Média?");
+
+        if (resposta == 0){
+            if (media >= 50){
+                if (media >= 90){
+                    JOptionPane.showMessageDialog(null, "Você está APROVADO com Sucesso, PARABÉNS!");
+                } else {
+                    JOptionPane.showMessageDialog(null, "Você está APROVADO!");
+                }
+            }else {
+                JOptionPane.showMessageDialog(null, "Você está REPROVADO!");
+            }
+        }else{
+            System.out.println("Não quiz ver a média!");
+        }
+
+    }
+
+}
